d78fbc3d7b94489c918832d17a9b351f15350f2a
KhoiPN <khoipn@KhoiPNs-MacBook-Pro.local>
2019-07-20 11:39:04 -0700

    config database

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 var config = require('./config.json');
 module.exports = {
     getDBConnectionString: function () {
-        return `mongodb://${config.username}:${config.password}@localhost/${config.db_name}`;
+        return `mongodb://${config.username}:${config.password}@${config.host}:${config.port}/${config.db_name}`;
     }
 }
